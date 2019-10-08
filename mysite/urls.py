fac04da93c21040043e801129d64f5e344db7b65
dawnik <dawnik@dawnik>
2019-10-08 19:18:40 +0200

    set from re_path for url

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    re_path('', include('users.urls'))
+    url('', include('users.urls'))
 ]
--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from django.urls import re_path
 
 
 urlpatterns = [
-    re_path('', views.index, name='index'),
-    re_path('register/', views.register_view, name='register_view', )
+    url('', views.index, name='index'),
+    url('register/', views.register_view, name='register_view', )
 ]
